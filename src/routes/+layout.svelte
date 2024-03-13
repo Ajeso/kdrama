bcf2323c177ddd11defae63ebd85645e8d6c828d
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2024-03-13 12:00:51 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+	import Header from './Components/Header.svelte';
+	import { siteName } from './+page.server.js';
+	let headerData = {
+		siteName: siteName,
+		navLinks: [{ text: 'dramacool.pa', url: 'https://dramacool.pa' }]
+	};
+</script>
+
+<div class="container bg-sky-300 p-7">
+	<Header {headerData} />
+	<main>
+		<slot />
+	</main>
+</div>
